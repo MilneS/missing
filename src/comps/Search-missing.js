c2bd0bf3c45c0126f29a010465826dc9ff4522be
Samira Milne <milne.samira@GMAIL.COM>
2021-08-18 15:01:50 -0500

    clear input field

--- src/comps/Search-missing.js
+++ src/comps/Search-missing.js
@@ -5,29 +5,33 @@ import Card from "./Missing-card";
 const Search = (props) => {
   const people = props.people;
 
-  // store select value
+  // select value
   const [optionSelected, setOptionSelected] = useState();
   const selectHandler = (e) => {
     setOptionSelected(e.target.value);
   };
-  
-  // store input value
+
+  // input value
   const [inputValue, setInputValue] = useState();
+  const[clearInput, setClearInput]=useState()
   const inputHandler = (e) => {
-    let valeur=e.target.value.charAt(0).toUpperCase()+e.target.value.slice(1)
+    let valeur =
+      e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1);
     setInputValue(valeur);
   };
 
   // filter comparing api at select value with input value
-  const [showTitle, setShowTitle] = useState(false);
+  const [hideTitle, setHideTitle] = useState(false);
   const [cardData, setCardData] = useState([]);
+
   const searchHandler = (e) => {
     e.preventDefault();
     const foundPeople = people.filter((person) => {
       return person[optionSelected] === inputValue;
     });
     setCardData(foundPeople);
-    setShowTitle(true);
+    setHideTitle(true);
+    setClearInput('')
   };
 
   return (
@@ -35,35 +39,48 @@ const Search = (props) => {
       <div className={classes.formcontainer}>
         <form onSubmit={searchHandler}>
           <select onChange={selectHandler} className={classes.formselect}>
+            <option value="none" selected disabled hidden>
+              Select
+            </option>
             <option value="first_name">First name</option>
             <option value="last_name">Last name</option>
-            <option value="city">City</option>
           </select>
           <label htmlFor="search" />
           <input
             onChange={inputHandler}
             id="search"
             className={classes.inputstyle}
+            value={clearInput}
           />
           <button className={classes.formbutton}>SEARCH</button>
         </form>
       </div>
-      {!showTitle && (
-        <div className={classes.titlecontainer}>Find a missing person in California.</div>
+      {!hideTitle && (
+        <div className={classes.titlecontainer}>
+          Find a missing person in California.
+        </div>
       )}
-      {showTitle && <div className={`${classes.divcardarea}`}> <div className={`${classes.cardarea}`}>
-        {/* map through arr, return Card component */}
-        {cardData &&
-          cardData.map((person, index) => {
-            return (
-              <div key={index} className={classes.card}>
-                <Card person={person} />
+      {hideTitle && (
+        <div className={`${classes.divcardarea}`}>
+          <div className={`${classes.cardarea}`}>
+            {/* map through arr, return Card component */}
+            {cardData &&
+              cardData.map((person, index) => {
+                return (
+                  <div key={index} className={classes.card}>
+                    <Card person={person} />
+                  </div>
+                );
+              })}
+            {cardData.length === 0 && (
+              <div className={classes.notfoundmessage}>
+                No registered missing person under this name.
               </div>
-            );
-          })}
-      </div>
-      </div>
- } </>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
