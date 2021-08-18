4f8fe6ef5465132227e2c23f67173e446eabeba7
Samira Milne <milne.samira@GMAIL.COM>
2021-08-18 17:08:29 -0500

    New person form

--- src/comps/New-missing.js
+++ src/comps/New-missing.js
@@ -1,7 +1,25 @@
-
-const NewMissing=()=>{
-
-    return 
-}
+const NewMissing = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="first_name">
+          <input id="first_name">First Name: </input>
+        </label>
+        <label htmlFor="last_name">
+          <input id="last_name">Last Name: </input>
+        </label>
+        <label htmlFor="age">
+          <input id="age">Age: </input>
+        </label>
+        <label htmlFor="city">
+          <input id="city">City: </input>
+        </label>
+        <label htmlFor="circumstance">
+          <input id="circumstance">Circumstance: </input>
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default NewMissing;
